d984b93a0cb2decc2e89cedf992a1a9805c37ea0
supriya.shahi <supriya.shahi@DEL1-LHP-N77869>
2025-09-24 19:58:44 +0530

    signUp Form For Seller

--- E-CommerceProject/src/app/seller-auth/seller-auth.ts
+++ E-CommerceProject/src/app/seller-auth/seller-auth.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-seller-auth',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './seller-auth.html',
   styleUrl: './seller-auth.css'
 })
 export class SellerAuth {
-
+signUp(data : object): void{
+console.warn(data);
+}
 }
